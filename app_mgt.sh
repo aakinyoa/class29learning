020889175589c8bd5f0f9b6ac269170af54a91d7
aakinyoa <aakinyoade@gmail.com>
2022-08-20 13:35:10 -0400

    install apache server

---
+++ app_mgt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#--To uninstall apache server--
+
+#sudo yum remove httpd
+#sudo systemctl disable httpd
+#sudo systemctl restart
+
+# - To install apache server, create Google and Yahoo directory as well as liz.html
+#sudo yum install httpd -y
+#sudo systemctl enable httpd.service
+#sudo systemctl start httpd.service
+#sudo systemctl restart httpd
+#sudo systemctl status httpd
+#sudo mkdir /var/www/html/Google
+#sudo mkdir /var/www/html/Yahoo
+#echo " God is forever faithful to His promises" > /var/www/html/liz.html
+#echo
+#echo "Learning git for distributed control versioning"
+#echo
