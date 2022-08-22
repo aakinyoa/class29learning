20f62947752e3caf10187bb7745e9d15dc503756
aakinyoa <aakinyoade@gmail.com>
2022-08-22 19:14:56 -0400

    loop structure

---
+++ loops.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "========== For loop ======================"
+for ((i=90;i<=100;i++))
+do 
+	echo $i
+done
+
+#==========================================
+#Writing similar script using while loop
+#rc=90
+#while (($rc<=100))
+#do 
+	#echo $rc
+	#rc=`expr $rc + 1`
+#done	
+
+
+	
